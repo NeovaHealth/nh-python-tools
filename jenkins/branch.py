719bb9b4da6101ca56f1912fa4142772cf1a8589
Will Earp <will.earp@icloud.com>
2016-02-25 14:22:07 +0000

    Added base64string. 	modified:   jenkins/branch.py

--- jenkins/branch.py
+++ jenkins/branch.py
@@ -2,6 +2,7 @@
 import json
 import re
 import urllib2
+import base64
 
 from string import punctuation
 from collections import namedtuple
@@ -137,15 +138,14 @@ class PropertiesBuilder(object):
 
     def is_branch(self, branch_name, repository):
         """Checks if repository has a given remote branch."""
-        url = "https://api.github.com/repos/NeovaHealth/" + repository + \
+        url = "https://api.github.com/repos/neovahealth/" + repository + \
               "/branches/" + branch_name
-
+        base64string = base64.encodestring(self._token).replace('\n', '')
         request = urllib2.Request(url)
-        request.add_header(
-            'Authorization', 'token ' + self._token)
+        request.add_header("Authorization", "Basic %s" % base64string)
 
         try:
-            print urllib2.urlopen(url).read()
+            urllib2.urlopen(request)
         except urllib2.HTTPError:
             result = False
         else:
